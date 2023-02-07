ed6db7174be3c7a71c1a723148073a79a32cfc93
christian <arthurchristian111@gmail.com>
2023-02-07 14:39:24 +0000

    added validations to registration input

--- src/app/modules/authentication/partials/registration/registration.component.ts
+++ src/app/modules/authentication/partials/registration/registration.component.ts
@@ -9,7 +9,33 @@ import { Validators } from '@angular/forms';
   styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  registration!: FormGroup;
+  show: boolean = false;
+
   constructor(private formBuilder: FormBuilder) {}
 
-  ngOnInit(): void {}
+  registrationForm(): void {
+    this.registration = this.formBuilder.group({
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.email,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+.[a-z]{2,4}$'),
+        ],
+      ],
+      username: ['', [Validators.required]],
+      firstName: ['', [Validators.required]],
+      lastName: ['', [Validators.required]],
+      password: ['', [Validators.required, Validators.minLength(8)]],
+    });
+  }
+
+  ngOnInit(): void {
+    this.registrationForm();
+  }
+
+  togglePassword() {
+    this.show = !this.show;
+  }
 }
