73b688c32d4060a6a3d4df03ef040b4ba8973b53
christian <arthurchristian111@gmail.com>
2023-02-13 13:07:55 +0000

    added socials to auth

--- src/app/modules/authentication/authentication.module.ts
+++ src/app/modules/authentication/authentication.module.ts
@@ -3,15 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { AuthenticationRoutingModule } from './authentication-routing.module';
 import { AuthenticationComponent } from './authentication.component';
-
+// import { AppModule } from "../../app.module";
 
 @NgModule({
-  declarations: [
-    AuthenticationComponent
-  ],
-  imports: [
-    CommonModule,
-    AuthenticationRoutingModule
-  ]
+  declarations: [AuthenticationComponent],
+  imports: [CommonModule, AuthenticationRoutingModule],
 })
-export class AuthenticationModule { }
+export class AuthenticationModule {}
