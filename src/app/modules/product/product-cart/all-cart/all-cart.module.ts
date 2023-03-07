d1098f92bfda34b091589b2c0d259d951f01df75
christian <arthurchristian111@gmail.com>
2023-03-07 23:57:29 +0000

    footer

--- src/app/modules/product/product-cart/all-cart/all-cart.module.ts
+++ src/app/modules/product/product-cart/all-cart/all-cart.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AllCartRoutingModule } from './all-cart-routing.module';
 import { AllCartComponent } from './all-cart.component';
+import { LayoutsModule } from '../../../../layouts/layouts.module';
 
 @NgModule({
   declarations: [AllCartComponent],
-  imports: [CommonModule, AllCartRoutingModule],
+  imports: [CommonModule, AllCartRoutingModule, LayoutsModule],
 })
 export class AllCartModule {}
